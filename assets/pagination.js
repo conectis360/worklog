4acfe7914f5137bd25bc26f3ca17b400f0f8c433
James Garcia <conectis360@gmail.com>
2024-11-16 23:58:36 -0300

    feat: table

--- assets/pagination.js
+++ assets/pagination.js
@@ -21,15 +21,16 @@ function renderWorklogs(worklogs) {
         const titleStyle = worklog.is_finished ? 'text-decoration: line-through;' : '';
 
         const worklogItem = `
-            <div class="box" id="worklog-${worklog.id}">
-                <h2 class="subtitle" style="${titleStyle}">${worklog.title}${isFinished} (Task #${worklog.task_number})</h2>
-                <p><a href="${worklog.description_link}" target="_blank" class="is-link">View Task Description</a></p>
-                <p><strong>Description:</strong> ${worklog.description}</p>
-                <p><strong>Analysis:</strong> ${worklog.analysis}</p>
-                <p class="has-text-grey-light">Created on: ${worklog.date_created}</p>
-                <button class="button is-info" onclick="showEditModal(${worklog.id})">Edit</button>
-                <button class="button is-danger" onclick="deleteWorklog(${worklog.id})">Delete</button>
-            </div>
+            <tr>
+                    <td></td>
+                    <td style="${titleStyle}">${worklog.title}${isFinished} (Task #${worklog.task_number})</td>
+                    <td><a href="${worklog.description_link}" target="_blank" class="is-link">View Task Description</a></td>
+                    <td>${worklog.description}</td>
+                    <td>${worklog.analysis}</td>
+                    <td>${worklog.date_created}</td>
+                    <td><button class="button is-info" onclick="showEditModal(${worklog.id})">Edit</button></td>
+                    <td><button class="button is-danger" onclick="deleteWorklog(${worklog.id})">Delete</button></td>
+                </tr>
         `;
         worklogList.innerHTML += worklogItem;
     });
