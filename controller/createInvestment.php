a4398a26e23c9575c099651f41cf6c81fe0b40d3
James Garcia <conectis360@gmail.com>
2024-11-27 23:03:52 -0300

    feat: investment

---
+++ controller/createInvestment.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../model/BudgetModel.php';
+
+$model = new BudgetModel();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    if (!isset($data['type'], $data['amount'], $data['category'], $data['date'])) {
+        http_response_code(400);
+        echo json_encode(['success' => false, 'message' => 'Invalid data']);
+        exit;
+    }
+
+    $success = $model->addInvestment(1, $data['amount'], $data['type'], $data['category'], $data['date'], $data['notes'] ?? '');
+    echo json_encode(['success' => $success !== false]);
+}
+?>
